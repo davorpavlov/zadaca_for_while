0f96dcdf088e33be13a4f4f53520f7888acdda23
davorpavlov <pavlovic.davor91@gmail.com>
2023-03-13 09:06:10 +0100

    Create rps_game.py

---
+++ rps_game.py
@@ -0,0 +1,32 @@
+import random
+
+while True:
+    print('Dobro došli u igru kamen-škare-papir!')
+    print('Odaberite vaš izbor: ')
+
+    options = ['kamen', 'škare', 'papir']
+    player_move = input('Kamen, škare ili papir? ')
+    computer_move = random.choice(options)
+
+    print('Računalo je odabralo:', computer_move)
+
+    if player_move == computer_move:
+        print('Rezultat je neriješen!')
+    elif player_move == 'kamen':
+        if computer_move == 'škare':
+            print('Pobjedili ste! Kamen tuče škare!')
+        else:
+            print('Izgubili ste! Papir prekriva kamen!')
+    elif player_move == 'škare':
+        if computer_move == 'papir':
+            print('Pobjedili ste! Škare režu papir!')
+        else:
+            print('Izgubili ste! Kamen tuče škare!')
+    elif player_move == 'papir':
+        if computer_move == 'kamen':
+            print('Pobjedili ste! Papir prekriva kamen!')
+        else:
+            print('Izgubili ste! Škare režu papir!')
+    play_again = input("Želite li ponovno igrati? (da/ne) ")
+    if play_again.lower() != "da":
+        break
